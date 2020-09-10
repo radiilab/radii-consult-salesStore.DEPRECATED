5c28ba96efe844eca21f460158906c842c02c845
Aditya Sen <aloysen951@gmail.com>
2020-09-10 13:41:29 +0530

    query to be sent to fire console

--- src/js/app.js
+++ src/js/app.js
@@ -18,3 +18,52 @@ const buttonRndRange = document.querySelector('#randomRange');
 
 buttonRndInt.addEventListener('click', outputRandomInt);
 buttonRndRange.addEventListener('click', outputRandomRange);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('queryMessage');
+
+// Listen for form submit
+document.getElementById('queryForm').addEventListener('submit', submitQueryForm);
+
+// Submit form
+function submitQueryForm(e){
+    e.preventDefault();
+  
+    // Get values
+    var name = getInputVal('name');
+    var company = getInputVal('company');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+  
+    // Save message
+    saveMessage(name, company, email, phone, message);
+  
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+  
+    // Hide alert after 3 seconds
+    setTimeout(function(){
+      document.querySelector('.alert').style.display = 'none';
+    },3000);
+  
+    // Clear form
+    document.getElementById('queryForm').reset();
+  }
+  
+  // Function to get get form values
+  function getInputVal(id){
+    return document.getElementById(id).value;
+  }
+  
+  // Save message to firebase
+  function saveMessage(name, company, email, phone, message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      name: name,
+      company:company,
+      email:email,
+      phone:phone,
+      message:message
+    });
+  }
